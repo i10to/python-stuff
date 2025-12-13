f467e990a83dde15fc8e89ff14b5d6f4b619ce40
i10to <147587684+i10to@users.noreply.github.com>
2025-12-13 08:56:51 +0000

    another kata and solutions added

--- codewars/Convert_number_to_reversed_array_of_digits.py
+++ codewars/Convert_number_to_reversed_array_of_digits.py
@@ -1,6 +1,6 @@
 #Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 #Example (Input => Output): #1. 35231 => [1,3,2,5,3]   #2. 0 => [0]
-
+#below is the first solution
 def digitize(n):  
     digit = [int(d) for d in str(n)] #change the n into a string and keep in under d variable, then change it into an integer
     nsl = []                         #list for storing the reverse digit
@@ -10,5 +10,8 @@ def digitize(n):
         j -= 1
     return(nsl)
 
+#below is the second solution
+def digitize(n):
+    return = [int(d) for d in str(n)[::-1]] #[::-1] tells python to reverse a string
 digitize(35231)
 digitize(0)
---
+++ codewars/Reversed_Strings.py
@@ -0,0 +1,6 @@
+#Complete the solution so that it reverses the string passed into it.
+
+#'world'  =>  'dlrow'
+#'word'   =>  'drow'
+def solution(string):
+    return(string[::-1]) #remember that [::-1] reverses a string
